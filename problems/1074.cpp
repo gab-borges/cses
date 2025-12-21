27477ad36d9a4a2e95f7f1a145a0deab7c0ad206
gab-borges <soliid002@gmail.com>
2025-12-21 17:20:18 -0300

    1074

---
+++ problems/1074.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+#define debug(x) cout << #x << " = " << x << '\n';
+#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << ' '; cout << '\n';
+#define vvdebug(a) cout << #a <<" ="<<endl; for(auto &row:a){for(auto &x:row) cout<<x<<' '; cout << endl;}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vi A(n);
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    sort(A.begin(), A.end());
+
+    ll meio = A[n/2];
+
+    ll pontos = 0;
+    for (int i = 0; i < n; i++) {
+        pontos += abs(A[i] - meio);
+    }
+
+    cout << pontos << '\n';
+
+    return 0;
+}
